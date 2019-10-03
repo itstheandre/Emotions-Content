231ee42a12079fc83a80f56a88682f4f0c82992f
Andre <itstheandre@gmail.com>
2019-10-03 11:22:18 +0200

    about to start working on routes, mofo

---
+++ models/Content.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const contentSchema = new Schema({
+  contentType: {
+    enum: ["text", "audio", "image", "video"]
+  },
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
+  views: Number
+});
+
+const Content = model("Content", contentSchema);
+module.exports = Content;
