0ef29728f359e8b616e804fc13c0bfdad511a341
Marco Accardi <contact.marco.accardi@gmail.com>
2019-10-08 14:17:12 +0200

    faceapi complete

--- client/src/components/UserViews/DisplayPost.js
+++ client/src/components/UserViews/DisplayPost.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import FaceApi from "../face-api/FaceApi";
-
 export default class DisplayPost extends Component {
   componentDidMount = () => {
     this.props.newView();
   };
 
   render() {
-    console.log(this.props);
     return (
       <div>
+        
         <Link to={`/u/${this.props.owner}`}>Check the creator's page</Link>
         <h1>{this.props.title}</h1>
         <h3>{this.props.owner}</h3>
         {this.props.body}
-<<<<<<< HEAD
-        <FaceApi />
-=======
-        <FaceApi id={this.props.id}/>
->>>>>>> e33c13a07a43f1a1fe7b5240eb56d397b81e560e
+        <FaceApi id={this.props.id} />
       </div>
     );
   }
--- client/src/components/face-api/FaceApi.js
+++ client/src/components/face-api/FaceApi.js
@@ -1,27 +1,128 @@
 import React, { Component } from "react";
 import * as faceapi from "face-api.js";
 import axios from "axios";
+import { Beforeunload } from "react-beforeunload";
+
+// MEDIAN FUNCTION to have more accurate result from the emotions
+const median = values => {
+  if (values.length === 0) return 0;
+
+  values.sort((a, b) => {
+    return a - b;
+  });
+
+  var half = Math.floor(values.length / 2);
+
+  if (values.length % 2) return values[half];
+
+  return (values[half - 1] + values[half]) / 2.0;
+};
+//-------------------------------------------------------------------
+
+//_______________________CHECK TAB FOCUS_____________________________
+let hidden = null;
+let visibilityChange = null;
+if (typeof document.hidden !== "undefined") {
+  // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+// __________________________________________________________________
 
 export default class FaceApi extends Component {
+  // SAVE TAB FOCUS
   state = {
-    angryMaxValue: 0,
-    disgustedMaxValue: 0,
-    fearfulMaxValue: 0,
-    happyMaxValue: 0,
-    neutralMaxValue: 0,
-    sadMaxValue: 0,
-    surprisedMaxValue: 0,
-    angryAvgValue: 0,
-    disgustedAvgValue: 0,
-    fearfulAvgValue: 0,
-    happyAvgValue: 0,
-    neutralAvgValue: 0,
-    sadAvgValue: 0,
-    surprisedAvgValue: 0,
-    gender: "",
-    age: 0,
-    time: 0
+    actions: []
   };
+
+  handleVisibilityChange = () => {
+    if (document[hidden]) {
+      this.setState({ actions: [...this.state.actions, "hide"] });
+    } else {
+      this.setState({ actions: [...this.state.actions, "show"] });
+    }
+  };
+
+  // MANAGES THE FACE DETECTION OBJ, STORE INFO TO DB, STOP FACEAPI DETECTION, CALCULATE MEDIAN AND MAX DETECTION VALUES, STOP WEBCAMS
+  dataManager = () => {
+    clearInterval(this.myDetection);
+    this.webcam();
+
+    // CALCULATE MAX AND AVG VALUES OF EMOTIONS DURING THE SESSION
+    const values = this.MaxAndAvg();
+    const getTime = this.timeSessionCalculation();
+    // STORE THE MAX AND AVG VALUES IN faceDetection Object
+    let faceDetection = {
+      angry: [values.angryMaxValue, values.angryAvgValue],
+      disgusted: [values.disgustedMaxValue, values.disgustedAvgValue],
+      fearful: [values.fearfulMaxValue, values.fearfulAvgValue],
+      happy: [values.happyMaxValue, values.happyAvgValue],
+      neutral: [values.neutralMaxValue, values.neutralAvgValue],
+      sad: [values.sadMaxValue, values.sadAvgValue],
+      surprised: [values.surprisedMaxValue, values.surprisedAvgValue],
+      age: Math.ceil(values.age),
+      gender: values.gender,
+      time: {
+        fullTime: values.time,
+        min: getTime.min,
+        sec: getTime.sec
+      }
+    };
+
+    console.log("FaceDetectionOBJ------", faceDetection);
+    console.log("STATE AFTER UNMOUNT-------", values);
+
+    // SEND faceDetection Object to the DB
+    axios
+      .put(`/u/api/${this.props.id}`, faceDetection)
+      .then(detectionValues => {
+        console.log(detectionValues);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  //_____________________TIME SESSION MANAGER____________________
+  timeElapsed = [];
+
+  timeSession = () => {
+    let currTime = "";
+    let startEndSession = [];
+    let date = new Date().toString().split(" ");
+    let day = date[2];
+    let month = date[1];
+    let year = date[3];
+    let time = date[4].toString().split("");
+    // let gmt = date[5];
+
+    let hour = time[0] + time[1];
+    let min = time[3] + time[4];
+    let sec = time[6] + time[7];
+    currTime = `${day} ${month} ${year} ${hour}:${min}:${sec}`;
+    // console.log(day, month, year, hour, min, sec);
+    startEndSession = this.timeElapsed.push(currTime);
+    return startEndSession;
+  };
+
+  timeSessionCalculation = () => {
+    let minStart = parseInt(this.timeElapsed[0].split(" ")[3].split(":")[1]);
+    let secStart = parseInt(this.timeElapsed[0].split(" ")[3].split(":")[2]);
+    let minEnd = parseInt(this.timeElapsed[1].split(" ")[3].split(":")[1]);
+    let secEnd = parseInt(this.timeElapsed[1].split(" ")[3].split(":")[2]);
+
+    let minElapsed = minEnd - minStart;
+    let secElapsed = secEnd - secStart;
+    return { min: minElapsed, sec: secElapsed };
+  };
+
+  //_______________________________________________________________
 
   // FACEAPI Detections
   angryValues = [];
@@ -34,9 +135,31 @@ export default class FaceApi extends Component {
   ageValues = [];
   genderValues = [];
 
+  // DETECTS THE EXACT GENDER
+  maleCount = 0;
+  femaleCount = 0;
+
+  exactGender = gender => {
+    gender &&
+      gender.forEach(x => {
+        if (x === "male") {
+          this.maleCount++;
+        } else if (x === "female") {
+          this.femaleCount++;
+        }
+      });
+    if (this.maleCount > this.femaleCount) {
+      return "male";
+    } else if (this.femaleCount > this.maleCount) {
+      return "female";
+    } else {
+      return "";
+    }
+  };
+
   // CALCULATE MAX AND AVG VALUES OF THE EMOTIONS ---- GENDER ---- AGE AVG
   MaxAndAvg = () => {
-    this.setState({
+    return {
       angryMaxValue: Math.max(...this.angryValues),
       disgustedMaxValue: Math.max(...this.disgustedValues),
       disgustedMaxValue: Math.max(...this.disgustedValues),
@@ -45,27 +168,35 @@ export default class FaceApi extends Component {
       neutralMaxValue: Math.max(...this.neutralValues),
       sadMaxValue: Math.max(...this.sadValues),
       surprisedMaxValue: Math.max(...this.surprisedValues),
-      angryAvgValue:
-        this.angryValues.reduce((x, y) => x + y, 0) / this.angryValues.length,
-      disgustedAvgValue:
-        this.disgustedValues.reduce((x, y) => x + y, 0) /
-        this.disgustedValues.length,
-      fearfulAvgValue:
-        this.fearfulValues.reduce((x, y) => x + y, 0) /
-        this.fearfulValues.length,
-      happyAvgValue:
-        this.happyValues.reduce((x, y) => x + y, 0) / this.happyValues.length,
-      neutralAvgValue:
-        this.neutralValues.reduce((x, y) => x + y, 0) /
-        this.neutralValues.length,
-      sadAvgValue:
-        this.sadValues.reduce((x, y) => x + y, 0) / this.sadValues.length,
-      surprisedAvgValue:
-        this.surprisedValues.reduce((x, y) => x + y, 0) /
-        this.surprisedValues.length,
-      age: this.ageValues.reduce((x, y) => x + y, 0) / this.ageValues.length,
-      gender: this.genderValues
-    });
+      angryAvgValue: median(this.angryValues),
+      // this.angryValues.reduce((x, y) => x + y, 0) / this.angryValues.length,
+      disgustedAvgValue: median(this.disgustedValues),
+      // this.disgustedValues.reduce((x, y) => x + y, 0) /
+      // this.disgustedValues.length,
+      fearfulAvgValue: median(this.fearfulValues),
+      // this.fearfulValues.reduce((x, y) => x + y, 0) /
+      // this.fearfulValues.length,
+      happyAvgValue: median(this.happyValues),
+      // this.happyValues.reduce((x, y) => x + y, 0) / this.happyValues.length,
+      neutralAvgValue: median(this.neutralValues),
+      // this.neutralValues.reduce((x, y) => x + y, 0) /
+      // this.neutralValues.length,
+      sadAvgValue: median(this.sadValues),
+      // this.sadValues.reduce((x, y) => x + y, 0) / this.sadValues.length,
+      surprisedAvgValue: median(this.surprisedValues),
+      // this.surprisedValues.reduce((x, y) => x + y, 0) /
+      // this.surprisedValues.length,2
+      age: median(this.ageValues),
+      ageArray: this.ageValues,
+      // mostOccurredAge: this.exactAge(this.ageValues),
+      // this.ageValues.reduce((x, y) => x + y, 0) / this.ageValues.length,
+      // gender: this.genderValues[10],
+      gender: this.exactGender(this.genderValues),
+      genderArray: this.genderValues,
+      maleNum: this.maleCount,
+      femaleNum: this.femaleCount,
+      time: this.timeElapsed
+    };
   };
 
   // variable for detection, to stop the detection when the component is unmounted
@@ -82,7 +213,7 @@ export default class FaceApi extends Component {
     );
   };
 
-  // Face API
+  //__________________________Face API________________________//
   faceApi = () => {
     const video = document.getElementById("video");
 
@@ -96,7 +227,7 @@ export default class FaceApi extends Component {
     ]).then(startVideo);
 
     // start webcam
-    // JUST WAIT A BIT THE CAMERA IS OFF BUT THE LED ON
+    // JUST WAIT A BIT: THE CAMERA IS OFF, BUT THE LED WILL STAY ON FOR A FEW SECs
     function startVideo() {
       navigator.getUserMedia(
         { video: {} },
@@ -104,6 +235,7 @@ export default class FaceApi extends Component {
         err => console.error(err)
       );
     }
+
     // append canvas on the video tag for the skeleton
     video.addEventListener("play", async () => {
       const canvas = await faceapi.createCanvasFromMedia(video);
@@ -167,50 +299,50 @@ export default class FaceApi extends Component {
       }, 100);
     });
   };
+  //__________________________________________________________//
 
   //////////////////////COMPONENTS-MOUNT////////////////////////
   componentDidMount = () => {
+    this.timeSession();
     this.faceApi();
+    //TAB FOCUS
+    document.addEventListener(
+      visibilityChange,
+      this.handleVisibilityChange,
+      false
+    );
   };
-  /////////////////////////FEATURE TO IMPLEMENT ///////////////////////////
-  // SEND DATA TO THE SERVER IF CLOSE TAB/REFRESH TAB/ADD TAB
+  //_____________________________________________________________
 
+  //////////////////////COMPONENTS-UNMOUNT////////////////////////
   componentWillUnmount = () => {
-    clearInterval(this.myDetection);
-    this.webcam();
+    // TAB FOCUS
+    document.removeEventListener(visibilityChange, this.handleVisibilityChange);
 
-    // CALCULATE MAX AND AVG VALUES OF EMOTIONS DURING THE SESSION
-    this.MaxAndAvg();
-
-    // STORE THE MAX AND AVG VALUES IN faceDetection Object
-    let faceDetection = {
-      angry: [this.state.angryMaxValue, this.state.angryAvgValue],
-      disgusted: [this.state.disgustedMaxValue, this.state.disgustedAvgValue],
-      fearful: [this.state.fearfulMaxValue, this.state.fearfulAvgValue],
-      happy: [this.state.happyMaxValue, this.state.happyAvgValue],
-      neutral: [this.state.neutralMaxValue, this.state.neutralAvgValue],
-      sad: [this.state.sadMaxValue, this.state.sadAvgValue],
-      surprised: [this.state.surprisedMaxValue, this.state.surprisedAvgValue],
-      age: Math.ceil(this.state.age),
-      gender: this.state.gender[10]
-    };
-    console.log("FaceDetectionOBJ------", faceDetection);
-    console.log("STATE AFTER UNMOUNT-------", this.state);
-
-    // SEND faceDetection Object to the DB
-    axios
-      .put(`/u/api/${this.props.id}`, faceDetection)
-      .then(detectionValues => {
-        console.log(detectionValues);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.timeSession();
+    this.timeSessionCalculation();
+    this.dataManager();
   };
+  //______________________________________________________________
 
+  ///////////////////////////RENDER///////////////////////////////
   render() {
+    //CHECK THE FOCUS OF THE TAB AND EITHER SEND DATA TO DB OR RESTART DETECTION
+    if (this.state.actions[this.state.actions.length - 1] === "hide") {
+      console.log("TAB FOCUS IS HIDDEN");
+      this.dataManager();
+    } else if (this.state.actions[this.state.actions.length - 1] === "show") {
+      this.faceApi();
+    }
     return (
       <div>
+        <Beforeunload
+          onBeforeunload={() => {
+            console.log("DISPLAY PROPS: ----", this.props);
+            this.dataManager();
+            return false;
+          }}
+        />
         <video
           // ref={this.videoTag}
           id="video"
@@ -219,15 +351,15 @@ export default class FaceApi extends Component {
           autoPlay
           muted
         ></video>
-        <button
+        {/* <button
           onClick={() => {
-            this.MaxAndAvg();
-            console.log("ANGRYVALUES", this.angryValues);
+            this.setState(this.MaxAndAvg());
+            // console.log("ANGRYVALUES", this.angryValues);
             console.log("STATE DURING THE SESSION -----------", this.state);
           }}
         >
           CHECK STATE
-        </button>
+        </button> */}
       </div>
     );
   }
