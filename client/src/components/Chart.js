c79fc56f7219f4195f8e19f6580993ebeae84753
Alfonso García-Miñaur <Alfonso@Alfonso-GMS.local>
2019-10-10 14:55:46 +0200

    fixing chart bugs

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -18,7 +18,6 @@ export default class Chart extends Component {
   };
   componentDidMount = () => {
     const user = this.props.user.username;
-    console.log(user);
     axios.get(`/api/chart/all/${user}`).then(response => {
       const allContent = response.data;
       console.log(allContent);
@@ -40,110 +39,114 @@ export default class Chart extends Component {
   };
 
   getData = viewsArr => {
-    const averageEmotion = viewsArr.map(el => {
-      return el.averageEmotion;
-    });
-    const maxEmotion = viewsArr.map(el => {
-      return el.maxEmotion;
-    });
-    function getEmotion(emotion) {
-      const sum = averageEmotion.reduce((acum, value) => {
-        if (value && value[emotion]) {
-          acum++;
-        }
-        return acum;
-      }, 0);
-      return sum;
-    }
-
-    const getAverage = array => {
-      let sum = array.reduce((previous, current) => (current += previous));
-      let avg = sum / array.length;
-      return avg;
-    };
-    function getMaxEmotion(emotion) {
-      const emotionArr = maxEmotion.map(el => {
-        return el[emotion];
+    if (!viewsArr.length) {
+      return;
+    } else {
+      const averageEmotion = viewsArr.map(el => {
+        return el.averageEmotion;
       });
-      return getAverage(emotionArr);
+      const maxEmotion = viewsArr.map(el => {
+        return el.maxEmotion;
+      });
+      function getEmotion(emotion) {
+        const sum = averageEmotion.reduce((acum, value) => {
+          if (value && value[emotion]) {
+            acum++;
+          }
+          return acum;
+        }, 0);
+        return sum;
+      }
+
+      const getAverage = array => {
+        let sum = array.reduce((previous, current) => (current += previous));
+        let avg = sum / array.length;
+        return avg;
+      };
+      function getMaxEmotion(emotion) {
+        const emotionArr = maxEmotion.map(el => {
+          return el[emotion];
+        });
+        return getAverage(emotionArr);
+      }
+      let neutralityArr = [];
+      averageEmotion.forEach(elem => {
+        neutralityArr.push(elem.neutralAvg);
+      });
+      let emotionalImpact = (100 - getAverage(neutralityArr)).toFixed(2);
+      console.log("Emo ", emotionalImpact);
+      let ageAverage = [];
+      viewsArr.forEach(elem => {
+        ageAverage.push(elem.age);
+      });
+      let timeArr = [];
+      // views.forEach(el => {
+      //   el.time.forEach(elem => {
+      //     elem.fullTime.forEach(single => {
+      //       single.push;
+      //     });
+      //   });
+      // });
+      console.log("Time-----", viewsArr[0].time);
+      let age = Math.floor(getAverage(ageAverage));
+      let genderArr = [];
+      viewsArr.forEach(elem => {
+        genderArr.push(elem.gender);
+      });
+      let male = genderArr.filter(el => {
+        if (el === "male") return el;
+      });
+      const malePercent = ((male.length / genderArr.length) * 100).toFixed(2);
+      const femalePercent = (
+        ((genderArr.length - male.length) / genderArr.length) *
+        100
+      ).toFixed(2);
+      const views = viewsArr.length;
+
+      const emotionsCount = {
+        angry: getEmotion("angryAvg"),
+        disgusted: getEmotion("disgustedAvg"),
+        fearful: getEmotion("fearfulAvg"),
+        happy: getEmotion("happyAvg"),
+        sad: getEmotion("sadAvg"),
+        surprised: getEmotion("surprisedAvg")
+      };
+
+      const chartData = {
+        labels: ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"],
+        datasets: [
+          {
+            label: "Emotions detected",
+            data: [
+              emotionsCount.angry,
+              emotionsCount.disgusted,
+              emotionsCount.fearful,
+              emotionsCount.happy,
+              emotionsCount.sad,
+              emotionsCount.surprised
+            ],
+            backgroundColor: [
+              "rgba(255, 95, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)"
+            ]
+          }
+        ]
+      };
+
+      this.updateState(
+        chartData,
+        emotionalImpact,
+        malePercent,
+        femalePercent,
+        age,
+        views
+      );
     }
-    let neutralityArr = [];
-    averageEmotion.forEach(elem => {
-      neutralityArr.push(elem.neutralAvg);
-    });
-    let emotionalImpact = (100 - getAverage(neutralityArr)).toFixed(2);
-    console.log("Emo ", emotionalImpact);
-    let ageAverage = [];
-    viewsArr.forEach(elem => {
-      ageAverage.push(elem.age);
-    });
-    let timeArr = [];
-    // views.forEach(el => {
-    //   el.time.forEach(elem => {
-    //     elem.fullTime.forEach(single => {
-    //       single.push;
-    //     });
-    //   });
-    // });
-    console.log("Time-----", viewsArr[0].time);
-    let age = Math.floor(getAverage(ageAverage));
-    let genderArr = [];
-    viewsArr.forEach(elem => {
-      genderArr.push(elem.gender);
-    });
-    let male = genderArr.filter(el => {
-      if (el === "male") return el;
-    });
-    const malePercent = ((male.length / genderArr.length) * 100).toFixed(2);
-    const femalePercent = (
-      ((genderArr.length - male.length) / genderArr.length) *
-      100
-    ).toFixed(2);
-    const views = viewsArr.length;
-
-    const emotionsCount = {
-      angry: getEmotion("angryAvg"),
-      disgusted: getEmotion("disgustedAvg"),
-      fearful: getEmotion("fearfulAvg"),
-      happy: getEmotion("happyAvg"),
-      sad: getEmotion("sadAvg"),
-      surprised: getEmotion("surprisedAvg")
-    };
-
-    const chartData = {
-      labels: ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"],
-      datasets: [
-        {
-          label: "Emotions detected",
-          data: [
-            emotionsCount.angry,
-            emotionsCount.disgusted,
-            emotionsCount.fearful,
-            emotionsCount.happy,
-            emotionsCount.sad,
-            emotionsCount.surprised
-          ],
-          backgroundColor: [
-            "rgba(255, 95, 132, 0.6)",
-            "rgba(54, 162, 235, 0.6)",
-            "rgba(255, 206, 86, 0.6)",
-            "rgba(75, 192, 192, 0.6)",
-            "rgba(153, 102, 255, 0.6)",
-            "rgba(255, 159, 64, 0.6)",
-            "rgba(255, 99, 132, 0.6)"
-          ]
-        }
-      ]
-    };
-
-    this.updateState(
-      chartData,
-      emotionalImpact,
-      malePercent,
-      femalePercent,
-      age,
-      views
-    );
   };
 
   updateState = (
@@ -174,39 +177,41 @@ export default class Chart extends Component {
   };
 
   render() {
+    console.log("SCARY SHIT INDEED, ", this.state);
     return (
       <>
         <h1>{this.state.title} </h1>
+        {this.state.content.length && (
+          <div className="chart">
+            <Bar
+              data={this.state.chartData}
+              options={{ maintainAspectRatio: false }}
+            />
 
-        <div className='chart'>
-          <Bar
-            data={this.state.chartData}
-            options={{ maintainAspectRatio: false }}
-          />
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <h5>Emotional Impact: {this.state.emotionalImpact}%</h5>
+              <h5>Average Age: {this.state.age} </h5>
+              <h5>Male:{this.state.malePercent}%</h5>
+              <h5>Female:{this.state.femalePercent}%</h5>
+              <h5>Total views:{this.state.views}</h5>
+            </div>
 
-          <div style={{ display: "flex", justifyContent: "space-around" }}>
-            <h5>Emotional Impact: {this.state.emotionalImpact}%</h5>
-            <h5>Average Age: {this.state.age} </h5>
-            <h5>Male:{this.state.malePercent}%</h5>
-            <h5>Female:{this.state.femalePercent}%</h5>
-            <h5>Total views:{this.state.views}</h5>
+            <button onClick={() => this.resetData()}>Show all Data</button>
+
+            {this.state.content.map(el => {
+              return (
+                <div>
+                  <h1>{el.title}</h1>
+                  <ContentInfo
+                    updateState={this.updateState}
+                    content={el}
+                    //   getData={this.getData}
+                  />
+                </div>
+              );
+            })}
           </div>
-
-          <button onClick={() => this.resetData()}>Show all Data</button>
-
-          {this.state.content.map(el => {
-            return (
-              <div>
-                <h1>{el.title}</h1>
-                <ContentInfo
-                  updateState={this.updateState}
-                  content={el}
-                  //   getData={this.getData}
-                />
-              </div>
-            );
-          })}
-        </div>
+        )}
       </>
     );
   }
--- client/src/components/ContentInfo.js
+++ client/src/components/ContentInfo.js
@@ -19,130 +19,134 @@ export default class Chart extends Component {
   };
 
   getData = viewsArr => {
-    console.log("ARRAY OF VIEWS???", viewsArr);
-    const averageEmotion = viewsArr.map(el => {
-      return el.averageEmotion;
-    });
-    const maxEmotion = viewsArr.map(el => {
-      return el.maxEmotion;
-    });
-    function getEmotion(emotion) {
-      const sum = averageEmotion.reduce((acum, value) => {
-        if (value && value[emotion]) {
-          acum++;
-        }
-        return acum;
-      }, 0);
-      return sum;
-    }
-
-    function getAverage(array) {
-      let sum = array.reduce((previous, current) => (current += previous));
-      let avg = sum / array.length;
-      return avg;
-    }
-    function getMaxEmotion(emotion) {
-      const emotionArr = maxEmotion.map(el => {
-        return el[emotion];
+    if (!viewsArr.length) {
+      return;
+    } else {
+      console.log("ARRAY OF VIEWS???", viewsArr);
+      const averageEmotion = viewsArr.map(el => {
+        return el.averageEmotion;
       });
-      return getAverage(emotionArr);
+      const maxEmotion = viewsArr.map(el => {
+        return el.maxEmotion;
+      });
+      function getEmotion(emotion) {
+        const sum = averageEmotion.reduce((acum, value) => {
+          if (value && value[emotion]) {
+            acum++;
+          }
+          return acum;
+        }, 0);
+        return sum;
+      }
+
+      function getAverage(array) {
+        let sum = array.reduce((previous, current) => (current += previous));
+        let avg = sum / array.length;
+        return avg;
+      }
+      function getMaxEmotion(emotion) {
+        const emotionArr = maxEmotion.map(el => {
+          return el[emotion];
+        });
+        return getAverage(emotionArr);
+      }
+      let neutralityArr = [];
+      averageEmotion.forEach(elem => {
+        neutralityArr.push(elem.neutralAvg);
+      });
+      let emotionalImpact = (100 - getAverage(neutralityArr)).toFixed(2);
+      console.log("Emo ", emotionalImpact);
+      let ageAverage = [];
+      viewsArr.forEach(elem => {
+        ageAverage.push(elem.age);
+      });
+      let age = Math.floor(getAverage(ageAverage));
+      console.log("Age ", age);
+      let genderArr = [];
+      viewsArr.forEach(elem => {
+        genderArr.push(elem.gender);
+      });
+      let male = genderArr.filter(el => {
+        if (el === "male") return el;
+      });
+      const malePercent = ((male.length / genderArr.length) * 100).toFixed(2);
+      const femalePercent = (
+        ((genderArr.length - male.length) / genderArr.length) *
+        100
+      ).toFixed(2);
+      const views = viewsArr.length;
+
+      const emotionsCount = {
+        angry: getEmotion("angryAvg"),
+        disgusted: getEmotion("disgustedAvg"),
+        fearful: getEmotion("fearfulAvg"),
+        happy: getEmotion("happyAvg"),
+        sad: getEmotion("sadAvg"),
+        surprised: getEmotion("surprisedAvg")
+      };
+
+      const avgChartData = {
+        labels: ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"],
+        datasets: [
+          {
+            label: "Emotions detected",
+            data: [
+              emotionsCount.angry,
+              emotionsCount.disgusted,
+              emotionsCount.fearful,
+              emotionsCount.happy,
+              emotionsCount.sad,
+              emotionsCount.surprised
+            ],
+            backgroundColor: [
+              "rgba(255, 95, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)"
+            ]
+          }
+        ]
+      };
+      const maxChartData = {
+        labels: ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"],
+        datasets: [
+          {
+            label: "Max emotion by %",
+            data: [
+              getMaxEmotion("angryMax") * 100,
+              getMaxEmotion("disgustedMax") * 100,
+              getMaxEmotion("fearfulMax") * 100,
+              getMaxEmotion("happyMax") * 100,
+              getMaxEmotion("sadMax") * 100,
+              getMaxEmotion("surprisedMax") * 100
+            ],
+            backgroundColor: [
+              "rgba(255, 95, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)"
+            ]
+          }
+        ]
+      };
+
+      this.setState({
+        avgChartData: avgChartData,
+        maxChartData,
+        age: age,
+        femalePercent: femalePercent,
+        malePercent: malePercent,
+        emotionalImpact: emotionalImpact,
+        views: views
+        //   content: content
+      });
     }
-    let neutralityArr = [];
-    averageEmotion.forEach(elem => {
-      neutralityArr.push(elem.neutralAvg);
-    });
-    let emotionalImpact = (100 - getAverage(neutralityArr)).toFixed(2);
-    console.log("Emo ", emotionalImpact);
-    let ageAverage = [];
-    viewsArr.forEach(elem => {
-      ageAverage.push(elem.age);
-    });
-    let age = Math.floor(getAverage(ageAverage));
-    console.log("Age ", age);
-    let genderArr = [];
-    viewsArr.forEach(elem => {
-      genderArr.push(elem.gender);
-    });
-    let male = genderArr.filter(el => {
-      if (el === "male") return el;
-    });
-    const malePercent = ((male.length / genderArr.length) * 100).toFixed(2);
-    const femalePercent = (
-      ((genderArr.length - male.length) / genderArr.length) *
-      100
-    ).toFixed(2);
-    const views = viewsArr.length;
-
-    const emotionsCount = {
-      angry: getEmotion("angryAvg"),
-      disgusted: getEmotion("disgustedAvg"),
-      fearful: getEmotion("fearfulAvg"),
-      happy: getEmotion("happyAvg"),
-      sad: getEmotion("sadAvg"),
-      surprised: getEmotion("surprisedAvg")
-    };
-
-    const avgChartData = {
-      labels: ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"],
-      datasets: [
-        {
-          label: "Emotions detected",
-          data: [
-            emotionsCount.angry,
-            emotionsCount.disgusted,
-            emotionsCount.fearful,
-            emotionsCount.happy,
-            emotionsCount.sad,
-            emotionsCount.surprised
-          ],
-          backgroundColor: [
-            "rgba(255, 95, 132, 0.6)",
-            "rgba(54, 162, 235, 0.6)",
-            "rgba(255, 206, 86, 0.6)",
-            "rgba(75, 192, 192, 0.6)",
-            "rgba(153, 102, 255, 0.6)",
-            "rgba(255, 159, 64, 0.6)",
-            "rgba(255, 99, 132, 0.6)"
-          ]
-        }
-      ]
-    };
-    const maxChartData = {
-      labels: ["Angry", "Disgusted", "Fearful", "Happy", "Sad", "Surprised"],
-      datasets: [
-        {
-          label: "Max emotion by %",
-          data: [
-            getMaxEmotion("angryMax") * 100,
-            getMaxEmotion("disgustedMax") * 100,
-            getMaxEmotion("fearfulMax") * 100,
-            getMaxEmotion("happyMax") * 100,
-            getMaxEmotion("sadMax") * 100,
-            getMaxEmotion("surprisedMax") * 100
-          ],
-          backgroundColor: [
-            "rgba(255, 95, 132, 0.6)",
-            "rgba(54, 162, 235, 0.6)",
-            "rgba(255, 206, 86, 0.6)",
-            "rgba(75, 192, 192, 0.6)",
-            "rgba(153, 102, 255, 0.6)",
-            "rgba(255, 159, 64, 0.6)",
-            "rgba(255, 99, 132, 0.6)"
-          ]
-        }
-      ]
-    };
-
-    this.setState({
-      avgChartData: avgChartData,
-      maxChartData,
-      age: age,
-      femalePercent: femalePercent,
-      malePercent: malePercent,
-      emotionalImpact: emotionalImpact,
-      views: views
-      //   content: content
-    });
   };
   //   componentDidMount = () => {
   //     this.props.getData(this.props.content.views);
@@ -161,13 +165,10 @@ export default class Chart extends Component {
   };
 
   render() {
-    console.log("ANOTHER: ", this.props);
-    console.log("THIS IS STATE: ", this.state);
     // this.getDato();
     return (
       <div>
         <div style={{ display: "flex", justifyContent: "space-around" }}>
-        
           <h4>Number of views {this.props.content.views.length} </h4>
 
           <h4>Date: {this.props.content.date}</h4>
@@ -178,7 +179,6 @@ export default class Chart extends Component {
           <button onClick={() => this.setNewData(this.state.maxChartData)}>
             Maximus
           </button>
-
         </div>
       </div>
     );
